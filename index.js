4c7e4629903b1cee83e454cc9fa2c66c64efb93d
YI KEE <marioxd1997@gmail.com>
2019-01-31 02:27:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  uppercase = string.toUpperCase()
+  var uppercase = string.toUpperCase()
   string = uppercase;
   console.log(uppercase);
 }
