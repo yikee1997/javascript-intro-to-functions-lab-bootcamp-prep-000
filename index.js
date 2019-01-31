9f48ffb63ad2f8cd3c3f5aebbb04978b3b322fee
YI KEE <marioxd1997@gmail.com>
2019-01-31 02:47:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return 'I can\'t hear you!';
   }
-  if (string.toUpperCase === string) {
+  if (string.toUpperCase() === string) {
     return 'YES INDEED!';
   }
   if (string === 'I love you, Grandma.') {
