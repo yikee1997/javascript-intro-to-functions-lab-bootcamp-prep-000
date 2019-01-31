b18d66aa951d2f307e6cbdca50e017514d82485f
YI KEE <marioxd1997@gmail.com>
2019-01-31 02:43:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ function logWhisper(string) {
   string = lowercase;
   console.log(lowercase);
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return 'I can\'t hear you!';
+  }
+  if (string.toUpperCase() === string) {
+    return 'YES INDEED!';
+  }
+  if (string === 'I love you, Grandma') {
+    return 'I love you, too';
+  }
+}
